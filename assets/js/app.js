0f12f8a02ef3c84769b847d22154d4be3a076a03
Michael F. Alvarez <michael_fred_alvarez@yahoo.com>
2020-12-17 19:32:18 -0800

    michael part completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,40 @@
 // [x] test fecth API call
 // apply to alchohol filter triggers
 
+
 const $ = (elm) => document.querySelector(elm);
+const $$ = (elm) => document.querySelectorAll(elm);
+
+/*
+
+
+
+*/
+
 const print = console.log;
 const BASE_URL = 'https://api.punkapi.com/v2/beers';
 
-async function fetchBeers () {
+const SM_ALC_URL = `${BASE_URL}/?abv_lt=4.6`;
+const MD_ALC_URL = `${BASE_URL}/?abv_gt=4.5&abv_lt=7.6`;
+const LG_ALC_URL = `${BASE_URL}/?abv_gt=7.5`;
+
+const SM_IBU_URL = `${BASE_URL}/?ibu_lt=35`;
+const MD_IBU_URL = `${BASE_URL}/?ibu_gt=34&ibu_lt=75`;
+const LG_IBU_URL = `${BASE_URL}/?ibu_gt=74`;
+
+const alcoholAll = $('#all-alcohol');
+const alcoholSm = $('#alcohol-sm');
+const alcoholMd = $('#alcohol-md');
+const alcoholLg = $('#alcohol-lg');
+
+const clearBeerResults = () => $('.beer-results').innerHTML = "";
+
+async function fetchBeers (url) {
+    clearBeerResults()
     try {
-        const response = await fetch(BASE_URL);
+        const response = await fetch(url);
         const data = await response.json();
+        print(data);
         
         for (const beer of data) {
             const { name,description,tagline,image_url,abv,ibu,food_pairing } = beer;
@@ -37,4 +63,9 @@ async function fetchBeers () {
     } catch (error) { console.error(error) }
 }
 
-fetchBeers()
+
+fetchBeers(BASE_URL)
+alcoholAll.addEventListener('click', () => fetchBeers(BASE_URL))
+alcoholSm.addEventListener('click', () => fetchBeers(SM_ALC_URL))
+alcoholMd.addEventListener('click', () => fetchBeers(MD_ALC_URL))
+alcoholLg.addEventListener('click', () => fetchBeers(LG_ALC_URL))
